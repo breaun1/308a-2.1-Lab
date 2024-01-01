80ea68369dc293b5fc61fe6503faa7aa7f4cf51e
breaun1 <breaun1@gmail.com>
2024-01-01 16:42:24 -0600

    "Created a companion for the adventurer"

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat",
+    catCompanion:
+        {
+        name: "Frank",
+        type:"Flea",
+        belongings:["small hat", "sunglasses"]
+     }
+    },
+    
+    }
