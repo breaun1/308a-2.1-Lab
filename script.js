eb94332a0aa36be2861c601ced0a32d16ac5c78f
breaun1 <breaun1@gmail.com>
2024-01-01 18:10:13 -0600

    created companion classes and reassigned them to the specified classes.

--- script.js
+++ script.js
@@ -1,17 +1,73 @@
 
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"],
-    companion: {
-        name: "Leo",
-        type: "Cat",
-    catCompanion:
-        {
-        name: "Frank",
-        type:"Flea",
-        belongings:["small hat", "sunglasses"]
-     }
-    },
+// const adventurer = {
+//     name: "Robin",
+//     health: 10,
+//     inventory: ["sword", "potion", "artifact"],
+//     companion: {
+//         name: "Leo",
+//         type: "Cat",
+//     catCompanion:
+//         {
+//         name: "Frank",
+//         type:"Flea",
+//         belongings:["small hat", "sunglasses"]
+//      }
+//     },
     
+//     }
+
+
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
     }
+
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+        }
+
+
+        
+  }
+
+const robin = new Adventurer("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Companion("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Companion("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+console.log(robin.companion.companion)
+
+
+class Adventurer extends Character {
+    constructor (name, role) {
+      super(name);
+      // Adventurers have specialized roles.
+      this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+  }
+
+  class Companion extends Adventurer{
+    constructor(name, specialPower, type){
+      this.name = name;
+      this.specialPower = specialPower;
+      this.type = type;
+    }
+
+  attack(){
+  console.log(`Gives ${this.name} the ability to attack all of your foes`)
+}
+  }
+  
